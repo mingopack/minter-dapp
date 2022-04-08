1b8b55de90024bbf1fe351c2f9fadc44fe56b82f
edo3000 <idomileo@hotmail.co.uk>
2022-04-09 02:50:02 +1000

    Initial Test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x99efBC56C95E3Bf9F2FA9f19fd19A6127dEDdf31"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
