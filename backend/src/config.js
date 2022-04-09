79a77f0273ed49e20fd736f66f199fed947ad7ff
edo3000 <idomileo@hotmail.co.uk>
2022-04-09 11:27:32 +1000

    file update

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,7 +65,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x52202d2c7345aD57cD0456AD8eAfFE1DA28a360f"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x52202d2c7345aD57cD0456AD8eAfFE1DA28a360f",]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
