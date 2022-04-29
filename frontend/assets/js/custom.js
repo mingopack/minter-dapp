fcbde2f226d057ac8b6ba89bde9f993c3b6f09bd
Rida Fatima <70073805+ridafatima21@users.noreply.github.com>
2022-04-29 23:41:41 +0500

    header responsive issue resolved

--- frontend/assets/js/custom.js
+++ frontend/assets/js/custom.js
@@ -5,3 +5,12 @@ $(window).scroll(function(){
        $('header').removeClass('scrolled');
     }
 });
+
+function responsive(maxWidth) {
+   if (maxWidth.matches) { 
+     $('#responsive').addClass('text-end');
+     $('#responsive').removeClass('align-items-center');
+   }
+ }
+  var maxWidth = window.matchMedia("(max-width: 991px)");
+  responsive(maxWidth);
