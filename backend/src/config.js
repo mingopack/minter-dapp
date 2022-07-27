51c439e95ea6afbad941d9381f76153eb0c055e7
edo3000 <idomileo@hotmail.co.uk>
2022-07-27 14:26:37 +0100

    price change

--- backend/src/config.js
+++ backend/src/config.js
@@ -55,7 +55,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0x52202d2c7345aD57cD0456AD8eAfFE1DA28a360f';
 const TREASURY_ADDRESS = '0x52202d2c7345aD57cD0456AD8eAfFE1DA28a360f';
 const MAX_SUPPLY = 5555; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
