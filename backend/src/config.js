a7b2c7fde8135545df481056a55fd9f5562a1210
edo3000 <idomileo@hotmail.co.uk>
2022-07-27 13:31:58 +0100

    test different token number

--- backend/src/config.js
+++ backend/src/config.js
@@ -56,7 +56,7 @@ const OWNER_ADDRESS = '0x52202d2c7345aD57cD0456AD8eAfFE1DA28a360f';
 const TREASURY_ADDRESS = '0x52202d2c7345aD57cD0456AD8eAfFE1DA28a360f';
 const MAX_SUPPLY = 5555; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = .00001; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-09-01T15:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
