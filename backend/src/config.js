bd80a04a42b4e3dc5f3e9043554091bb5015fb25
mingopack <103011955+mingopack@users.noreply.github.com>
2022-09-23 18:06:05 +0100

    Date update both

--- backend/src/config.js
+++ backend/src/config.js
@@ -58,10 +58,10 @@ const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Polygon = MATI
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-11-30T15:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-12-02T15:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-11-29T15:00:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-12-01T15:00:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royaltys address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x52202d2c7345aD57cD0456AD8eAfFE1DA28a360f"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
